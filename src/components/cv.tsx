fb1e7f2937f396a520c496239e68f03d69704a28
Daniel Marques <daniel.brz2009@gmail.com>
2023-05-29 23:57:48 -0300

    WIP

---
+++ src/components/cv.tsx
@@ -0,0 +1,10 @@
+export const CV = () => (
+	<div class="flex w-24 flex-col justify-end">
+		<a href="resume.pdf" download="Daniel Marques - Resume.pdf">
+			<div class="flex items-center gap-2 rounded-2xl border-2 border-black px-4 py-2 hover:bg-pink-500">
+				<p class="mt-[2px]">CV</p>
+				<img src="download.svg" alt="download my cv" />
+			</div>
+		</a>
+	</div>
+)
