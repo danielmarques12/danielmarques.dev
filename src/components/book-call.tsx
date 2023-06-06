8af336394ddf90b1b87ea0a0f0b67d7722f283ec
Daniel Marques <daniel.brz2009@gmail.com>
2023-06-06 01:10:07 -0300

    WIP

--- src/components/book-call.tsx
+++ src/components/book-call.tsx
@@ -6,6 +6,7 @@ export default function CalCom() {
 			<div className="mt-24 flex flex-col items-center gap-6">
 				<p className="text-2xl">Let's chat. Book a call!</p>
 				<Cal
+					title="Book a call with me on cal.com"
 					className="w-full"
 					calLink="danielmarques"
 					config={{
