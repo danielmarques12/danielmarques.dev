8d82a6613867d3114280060be688e7df398549de
Daniel Marques <daniel.brz2009@gmail.com>
2023-06-06 01:31:38 -0300

    WIP

--- src/components/book-call.tsx
+++ src/components/book-call.tsx
@@ -3,11 +3,11 @@ import Cal from '@calcom/embed-react'
 export default function CalCom() {
 	return (
 		<section>
-			<div className="mt-24 flex flex-col items-center gap-6">
+			<div className="mb-16 mt-16 flex flex-col items-center gap-6 md:mb-24 md:mt-24">
 				<p className="text-2xl">Let's chat. Book a call!</p>
 				<Cal
 					title="Book a call with me on cal.com"
-					className="w-full"
+					className="md:w-full"
 					calLink="danielmarques"
 					config={{
 						theme: 'light'
