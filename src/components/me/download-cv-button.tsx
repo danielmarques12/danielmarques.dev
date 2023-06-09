d69af7222022cc1d7a6a13c752305ff56199f1bb
Daniel Marques <daniel.brz2009@gmail.com>
2023-06-09 09:46:10 -0300

    open cv on a new tab instead of downloading it

--- src/components/me/download-cv-button.tsx
+++ src/components/me/download-cv-button.tsx
@@ -1,7 +1,7 @@
 export default function DownloadCVButton() {
 	return (
 		<div className="flex w-24 flex-col justify-end">
-			<a href="resume.pdf" download="Daniel Marques - Resume.pdf">
+			<a href="resume.pdf" target="_blank">
 				<div className="flex items-center gap-2 rounded-2xl border-2 border-black px-4 py-1 hover:bg-pink-200">
 					<p className="mt-[2px]">CV</p>
 					<img src="download.svg" alt="download my cv" />
