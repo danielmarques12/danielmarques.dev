3d9185dd6b6c83c8c19ca6558eb6c8c280f2098e
Daniel Marques <daniel.brz2009@gmail.com>
2023-06-13 12:55:17 -0300

    WIP

--- src/components/book-call.tsx
+++ src/components/book-call.tsx
@@ -2,7 +2,10 @@ import Cal from '@calcom/embed-react'
 
 export default function CalCom() {
 	return (
-		<section className="flex h-screen snap-start items-center justify-center">
+		<section
+			aria-label="book a call with me on cal.com"
+			className="flex h-screen snap-start items-center justify-center"
+		>
 			<div className="mb-16 mt-16 flex flex-col gap-6 md:mb-24 md:mt-24 md:w-full">
 				<p className="text-2xl">Let's chat. Book a call!</p>
 				<Cal
