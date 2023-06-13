0d69d2ef2fbc03e9fe51c625f5af8959cef89cf9
Daniel Marques <daniel.brz2009@gmail.com>
2023-06-13 13:06:37 -0300

    responsiviness fix

--- src/components/book-call.tsx
+++ src/components/book-call.tsx
@@ -4,7 +4,7 @@ export default function CalCom() {
 	return (
 		<section
 			aria-label="book a call with me on cal.com"
-			className="flex h-screen snap-start items-center justify-center"
+			className="flex snap-start items-center justify-center md:h-screen"
 		>
 			<div className="mb-16 mt-16 flex flex-col gap-6 md:mb-24 md:mt-24 md:w-full">
 				<p className="text-2xl">Let's chat. Book a call!</p>
